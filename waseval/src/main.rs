681241be7c77a34506a8c158da877a2eb62c84da
msakuta <masahiro.sakuta@gmail.com>
2025-11-30 03:09:04 +0900

    Experimental support of chibiwasm runtime in waseval

--- waseval/src/main.rs
+++ waseval/src/main.rs
@@ -1,11 +1,62 @@
 use std::{cell::RefCell, sync::Mutex};
 
 use wasmtime::*;
+use chibiwasm::{module::ExternalFuncInst, Importer, Runtime, Value};
 
 struct MyState;
 
 fn main() -> Result<()> {
     println!("Compiling module...");
+
+    let use_wasmtime = false;
+
+    if use_wasmtime {
+        run_wasmtime()?;
+    } else {
+        run_chibiwasm()?;
+    }
+
+    Ok(())
+}
+
+struct Imported {
+    name: String,
+}
+
+impl Importer for Imported {
+    fn invoke(
+            &self,
+            _store: std::rc::Rc<RefCell<chibiwasm::Store>>,
+            func: chibiwasm::module::ExternalFuncInst,
+            args: Vec<Value>,
+        ) -> Result<Option<Value>> {
+            if func.field.as_str() == "putc" {
+                if let Value::I32(val) = args[0] {
+                    println!("{val}");
+                }
+            }
+            Ok(None)
+    }
+
+    fn name(&self) -> &str {
+        dbg!("wasi_ephemeral_nn")
+    }
+}
+
+fn run_chibiwasm() -> anyhow::Result<()> {
+    let mut runtime = Runtime::from_file("../wascal.wasm", Some(vec![
+        Box::new(Imported {
+            name: "output".to_string(),
+        })
+    ]))?;
+    println!("chibiwasm Runtime instantiated!");
+    if let Some(output) = runtime.call("main".into(), vec![Value::I32(10), Value::I32(100)])? {
+        println!("output: {}", output);
+    }
+    Ok(())
+}
+
+fn run_wasmtime() -> Result<()> {
     let engine = Engine::default();
     let module = Module::from_file(&engine, "../wascal.wasm")?;
 
@@ -36,6 +87,5 @@ fn main() -> Result<()> {
 
     println!("Calling export...");
     run.call(&mut store, ())?;
-
     Ok(())
 }
